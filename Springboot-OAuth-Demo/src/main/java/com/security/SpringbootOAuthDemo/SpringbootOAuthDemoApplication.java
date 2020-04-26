5d8311766894bb1a4686d0cb1bd2e94a8b8e53fd
Rishikesh Mandal <me.rishi29@gmail.com>
2020-04-26 18:54:05 -0400

    Spring Boot OAuth Demo Application

---
+++ Springboot-OAuth-Demo/src/main/java/com/security/SpringbootOAuthDemo/SpringbootOAuthDemoApplication.java
@@ -0,0 +1,27 @@
+package com.security.SpringbootOAuthDemo;
+
+import java.security.Principal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableOAuth2Sso
+@RestController
+public class SpringbootOAuthDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootOAuthDemoApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String message(Principal principal) {
+
+		return "Hi " + principal.getName() + " Welcome to Spring Boot OAuth Demo Application";
+
+	}
+
+}
